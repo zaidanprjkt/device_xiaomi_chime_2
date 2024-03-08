b9cab4265904949f324d97381a89a6d28dc29544
Bruno Martins <bgcngm@gmail.com>
2024-03-08 21:40:30 +0900

    chime: Inherit from QTI FCM
    - Cleanup, properly split framework and device compability manifest

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -96,9 +96,11 @@ LOC_HIDL_VERSION := 4.0
 TARGET_HALS_PATH ?= hardware/qcom-caf/$(TARGET_BOARD_PLATFORM)
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
-DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
+DEVICE_MATRIX_FILE += $(COMMON_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+   $(COMMON_PATH)/device_compatibility_matrix.xml \
+   hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):init_chime
