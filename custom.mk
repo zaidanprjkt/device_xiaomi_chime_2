26aafb15e57bf5b82916bfc3f9f3842e344f00da
zaidannn7 <72336864+zaidannn7@users.noreply.github.com>
2024-04-22 18:15:30 +0700

    RisingOS-ify.
    Signed-off-by: zaidannn7 <72336864+zaidannn7@users.noreply.github.com>

--- custom.mk
+++ custom.mk
@@ -4,11 +4,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Derpfest stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# Officials Flag
-DERP_BUILDTYPE := Official
+# Rising flags
+RISING_CHIPSET="Snapdragon 662"
+RISING_MAINTAINER="scaledzdn"
+TARGET_ENABLE_BLUR := false
+WITH_GMS := false
