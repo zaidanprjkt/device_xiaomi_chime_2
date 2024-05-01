4f6c34eb0b5847d5f778e3a7f51ea6c49f60f06d
zaidannn7 <72336864+zaidannn7@users.noreply.github.com>
2024-05-01 13:31:54 +0700

    Crdroid Bringup.
    Signed-off-by: zaidannn7 <72336864+zaidannn7@users.noreply.github.com>

--- custom.mk
+++ custom.mk
@@ -10,8 +10,3 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# Rising flags
-RISING_CHIPSET="Snapdragon 662"
-RISING_MAINTAINER="scaledzdn"
-TARGET_ENABLE_BLUR := false
-WITH_GMS := false
